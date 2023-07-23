ef0b9f13a7e48b41c52c36888170ab02d6d7a1fc
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-07-23 20:41:29 +0200

    CRUD en users, relaciones

--- routers/user.py
+++ routers/user.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends
-from schemas.user import UserBase, UserDisplay
 from sqlalchemy.orm import Session
+from schemas.user import UserBase, UserDisplay
 from config.db import get_db
 from config import db_user
+from typing import List
 
 
 router = APIRouter(
@@ -14,3 +15,23 @@ router = APIRouter(
 @router.post('/', response_model=UserDisplay)
 def create_user(request: UserBase, db: Session = Depends(get_db)):
     return db_user.create_user(db, request)
+
+
+@router.get('/', response_model=List[UserDisplay])
+def get_users(db: Session = Depends(get_db)):
+    return db_user.get_all_users(db)
+
+
+@router.get('/{id}', response_model=UserDisplay)
+def get_user(id: int, db: Session = Depends(get_db)):
+    return db_user.get_user(db, id)
+
+
+@router.put('/{id}/update')
+def update_user(id: int, request: UserBase, db: Session = Depends(get_db)):
+    return db_user.update_user(db, id, request)
+
+
+@router.delete('/{id}')
+def delete_user(id: int, db: Session = Depends(get_db)):
+    return db_user.delete_user(db, id)
